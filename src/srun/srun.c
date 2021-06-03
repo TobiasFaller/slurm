fa97cabbd40548dd338103974da3e6f37d096687
Alejandro Sanchez <alex@schedmd.com>
2021-06-03 11:36:44 +0200

    Add srun --bcast-exclude option.
    This option is also available through the SLURM_BCAST_EXCLUDE input
    environment variable.
    
    It does nothing for now, in preparation for subsequent commits.
    
    Bug 11427.

--- src/srun/srun.c
+++ src/srun/srun.c
@@ -744,6 +744,7 @@ static int _file_bcast(slurm_opt_t *opt_local, srun_job_t *job)
 		if (sep)
 			sep[0] = ',';
 	}
+	params->exclude = xstrdup(srun_opt->bcast_exclude);
 	if (srun_opt->bcast_file && (srun_opt->bcast_file[0] == '/')) {
 		params->dst_fname = xstrdup(srun_opt->bcast_file);
 	} else if ((tmp = xstrcasestr(slurm_conf.bcast_parameters,
@@ -786,6 +787,7 @@ static int _file_bcast(slurm_opt_t *opt_local, srun_job_t *job)
 	}
 	slurm_destroy_selected_step(params->selected_step);
 	xfree(params->dst_fname);
+	xfree(params->exclude);
 	xfree(params->src_fname);
 	xfree(params);
 
